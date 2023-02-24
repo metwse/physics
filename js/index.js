fb485ba42b8deb2c470e456e7462a6f6e82b85d8
metwse <108795071+metwse@users.noreply.github.com>
2023-02-24 22:55:45 +0300

    1.2.0 - iframe & temporary loading screen

--- js/index.js
+++ js/index.js
@@ -3,29 +3,29 @@ const simulations = [
     ['Newton 1', '/simulations/physics-v1.html']
 ]
 
+
 const d = document, w = window
-var w2
+var w2, iframe
 
 const cache = {}
+const template = d.querySelector('template.simulation')
+const loading =  d.getElementById('loading')
 
 
+
+
+//{{{ HEADER nav 
 const header = Object.fromEntries(['select', 'reload', 'debug', 'tools'].map(v => [v, d.querySelector(`body > header .toolbar .${v}`)]))
 !['debug', 'tools'].forEach(v => {
-    const target = d.getElementById(v)
-    target.querySelector('.close').onclick = header[v].onclick = () => {
-        target.classList.toggle('active')
+    header[v].onclick = () => {
+        w2?.[v].parentNode.classList.toggle?.('active')
         header[v].classList.toggle('active')
     }
-    target.wrapper = target.querySelector('.wrapper')
-    target.wrapper.attachShadow({ mode: 'open' })
-    target.root = target.wrapper.shadowRoot
 })
 header.reload.onclick = () => {
     if (app.last === null) return
     app.loadURL(app.last)
 }
-
-
 for (let [i, simulation] of simulations.entries()) {
     let option = d.createElement('option')
     option.value = i, option.innerHTML = simulation[0]
@@ -35,6 +35,9 @@ header.select.oninput = ({ target: { value } }) => {
     if (header.select.children[0].className == 'placeholder') header.select.children[0].remove()
     app.loadURL(simulations[value][1])
 }
+//}}}
+
+
 
 
 const app = {
@@ -44,60 +47,68 @@ const app = {
         return callback => { if (this.random == random) requestAnimationFrame(() => callback()) }
     },
 
-    //TODO:: history & reload (loadURL > this.last)
+    //TO DO:: history & reload (loadURL > this.last)
     last: null,
     
     async loadURL(url) {
         var data = cache[url], elem = d.createElement('div'), random = Math.random()
         this.last = url
         this.random = random
+        loading.style.display = 'flex'
 
         if (!data) data = cache[url] = await fetch(url).then(r => r.text())
         if (this.random != random) return
         elem.innerHTML = data
 
-        var newCanvas = d.createElement('canvas')
-        newCanvas.width = 800, newCanvas.height = 600
-        canvas.replaceWith(newCanvas)
-        canvas = newCanvas, ctx = canvas.getContext('2d')
+        var newIframe = d.createElement('iframe')
+        iframe.replaceWith(newIframe)
+        iframe = newIframe
+         
+        w2 = iframe.contentWindow
+        w2.d = w2.document, w2.w = w2
+        //TO DO:: better animation manager
+        w2.console = console, w2.frame = w2.requestAnimationFrame = this.requestAnimationFrameFunction(random)
+
+        for (let [i, v] of ['head', 'body'].entries()) w2.d[v].innerHTML = template.content.children[i].innerHTML
+    
+        //TO DO:: improve perf
+        await new Promise(res => w2.onload = res)
+        /* onload */ w2.canvas = w2.d.querySelector('canvas')
+        /* onload */ w2.ctx = w2.canvas.getContext('2d')
 
         !['tools', 'debug'].forEach(v => { 
-            var html = elem.querySelector(v)?.innerHTML ?? false
-            header[v].style.display = w[v].style.display = html ? '' : 'none'
-            w[v].root.innerHTML = html ? `<div class="root">${html}</div>` : '' 
+            let html = elem.querySelector(v)?.innerHTML ?? false
+
+            w2[v] = w2.d.getElementById(v)
+            if (header[v].classList.contains('active')) w2[v].classList.add('active')
+            w2[v].querySelector('.title .close').onclick = header[v].onclick
+            header[v].style.display = w2[v].style.display = html ? '' : 'none'
+
+            w2[v].root = w2[v].querySelector('.wrapper').attachShadow({ mode: 'open' })
+            w2[v].root.innerHTML = html ? `<div class="root">${html}</div>` : ''
+            w2[v] = w2[v].children[0]
         })
-
-        w2 = newWindow()
-        w2.ctx = ctx, w2.canvas = canvas
-        w2.console = console, w2.frame = w2.requestAnimationFrame = this.requestAnimationFrameFunction(random)
-        w2.tools = tools.root.children[0], w2.debug = debug.root.children[0]
-
+        
         await Promise.all(
             Object.entries(eval(`() => { var v = {${elem.querySelector('script[requirements]')?.innerText ?? ''}}; return v }`)()).map(async ([key, value]) => {
-                var script = cache[value]
-                if (!script) script = cache[value] = await fetch(value).then(r => r.text())
+                var script = cache[value]; if (!script) script = cache[value] = await fetch(value).then(r => r.text())
                 if (this.random == random) { 
                     w2.eval(`window['${key.replace(/'/g, '\'')}'] = (() => { ${script}; if (typeof exports == 'undefined') var exports = undefined; return exports })()`)
-                    //TODO:: loading screen
+                    //TO DO:: better loading screen
                 }
                 return
             })
         )
         if (this.random != random) return
 
+        loading.style.display = ''
         w2.eval(elem.querySelector('script:not([requirements])')?.innerText ?? '')
     }
 }
 
-function newWindow() {
-    var iframe = d.createElement('iframe')
-    iframe.style.display = 'none', iframe.src = 'about:blank'
-    d.body.appendChild(iframe)
-    setTimeout(() => iframe.remove())
-    return iframe.contentWindow 
-}
+
+
 
 onload = () => {
-    w.canvas = d.querySelector('canvas')
-    w.ctx = w.canvas.getContext('2d')
+    iframe = d.querySelector('iframe')
 }
